ea3505bfef788864899ef2c5b6bccd7895c8eecd
Anna Ionova <echoespond@gmail.com>
2019-04-08 00:42:54 -0400

    data binding, events

--- app.js
+++ app.js
@@ -2,11 +2,32 @@ new Vue({
   el: '#vue-app',
   data: {
     name: 'Shaun',
-    job: 'Ninja'
+    job: 'Ninja',
+    website: 'http://annaionova.com',
+    websiteTag: '<a href="http://annaionova.com">My Website</a>',
+
+    age: 25,
+
+    x: 0,
+    y: 0,
   },
   methods: {
     greet: function(timeOfDay){
       return `Good ${timeOfDay} ${this.name}!`
+    },
+
+    add: function(inc) {
+      this.age += inc;
+    },
+
+    subtract: function(dec) {
+      this.age -= dec;
+    },
+
+    updateXY: function(e) {
+      this.x = e.offsetX;
+      this.y = e.offsetY;
     }
+
   }
 });
