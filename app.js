bfe5adc420db6f4e53268381316303f5bb821194
Anna Ionova <echoespond@gmail.com>
2019-04-08 23:05:42 -0400

    keybord events, event modifiers, two-way data binding, computed properties

--- app.js
+++ app.js
@@ -6,11 +6,24 @@ new Vue({
     website: 'http://annaionova.com',
     websiteTag: '<a href="http://annaionova.com">My Website</a>',
 
+
     age: 25,
+
 
     x: 0,
     y: 0,
+
+    // Two way data binding - Tutorial 8
+    nameDataBinding: '',
+    ageDataBinding: '',
+
+    // Computed Properties - Tutorial 9
+    a: 0,
+    b: 0,
+    ageForComputed: 20,
+
   },
+
   methods: {
     greet: function(timeOfDay){
       return `Good ${timeOfDay} ${this.name}!`
@@ -24,10 +37,44 @@ new Vue({
       this.age -= dec;
     },
 
+
     updateXY: function(e) {
       this.x = e.offsetX;
       this.y = e.offsetY;
-    }
+    },
 
+    click: function() {
+      alert('Cliiiickkked!');
+    },
+
+    // Keyboard Events - Tutorial 7
+    logName: function() {
+      console.log('NAMEEEE!');
+    },
+
+    logAge: function() {
+      console.log('AGEEEE!');
+    },
+
+
+    // Computed Properties - Tutorial 9
+    // addToA: function() {
+    //   return this.a + this.ageForComputed;
+    // },
+
+    // addToB: function() {
+    //   return this.b + this.ageForComputed;
+    // }
+  },
+
+  computed: {
+    // Computed Properties - Tutorial 9
+    addToA: function () {
+      return this.a + this.ageForComputed;
+    },
+
+    addToB: function () {
+      return this.b + this.ageForComputed;
+    }
   }
 });
