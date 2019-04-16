9e89a5747ebc3df0303d83b1fb2ee1f38ad3bc8d
Anna Ionova <echoespond@gmail.com>
2019-04-16 12:34:06 -0400

    vue cli, root component, nesting components, scoped css

---
+++ my-vue-proj/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <ninjas></ninjas>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'Ninja App'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+  h1 {
+    color: purple;
+  }
+
+</style>
---
+++ my-vue-proj/src/ninjas.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>List of Ninjas</h1>
+    <ol>
+      <li v-for="ninja in ninjas">{{ ninja }}</li>
+    </ol>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      ninjas: ['Yoshi', 'Mario', 'Ryu']
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+h1 {
+  color: green;
+}
+
+</style>
